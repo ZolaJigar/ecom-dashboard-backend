40b53ab992d444e4fb952dc20cbcc1bcec8f7c63
zolajigar <kingofdarkness012345@gmail.com>
2022-11-23 20:20:32 +0530

    list product api done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,16 +12,22 @@ class ProductController extends Controller
     //
     public $Product;
 
-    function addProduct(Request $req){
+    function addProduct(Request $req)
+    {
 
         // return $req->file('file')->store('products');
         $product = new Product;
-        $product->name=$req->input('name');
-        $product->price=$req->input('price');
-        $product->description=$req->input('description');
-        $product->file_path=$req->file('file')->store(('products')); //file_path is column in our database
+        $product->name = $req->input('name');
+        $product->price = $req->input('price');
+        $product->description = $req->input('description');
+        $product->file_path = $req->file('file')->store(('products')); //file_path is column in our database
         $product->save();
         return $product;
         // return $req->input();
+    }
+
+    function list(Request $req)
+    {
+        return Product::all();
     }
 }
