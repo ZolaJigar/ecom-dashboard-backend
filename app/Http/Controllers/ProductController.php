4922d926d554840685d4ced3f235ed56fb43aec1
zolajigar <kingofdarkness012345@gmail.com>
2022-11-23 19:39:00 +0530

    Add Product api done

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\RateLimiter\RequestRateLimiterInterface;
+use App\Models\Product;
+use GuzzleHttp\Handler\Proxy;
+
+class ProductController extends Controller
+{
+    //
+    public $Product;
+
+    function addProduct(Request $req){
+
+        // return $req->file('file')->store('products');
+        $product = new Product;
+        $product->name=$req->input('name');
+        $product->price=$req->input('price');
+        $product->description=$req->input('description');
+        $product->file_path=$req->file('file')->store(('products')); //file_path is column in our database
+        $product->save();
+        return $product;
+        // return $req->input();
+    }
+}
