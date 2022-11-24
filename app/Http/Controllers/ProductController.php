d246e0be2466265f4d1a6bd7fcbe1f95e32c2497
zolajigar <kingofdarkness012345@gmail.com>
2022-11-24 10:16:20 +0530

    Search api done for product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -44,4 +44,8 @@ class ProductController extends Controller
     function getProduct($id){
         return Product::find($id);
     }
+
+    function search($key){
+        return Product::where('name','Like',"%$key%")->get() ;
+    }
 }
